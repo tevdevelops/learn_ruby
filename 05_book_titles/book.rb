9a86aac0fa2f79febe400a77a7674981262955ea
Tevin Rivera <tevinrivera94@gmail.com>
2017-05-08 11:44:33 -0400

    Completed answers for 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title
+    words = @title.split
+    words.map do |word|
+      little_words = ['and','the', 'of', 'a', 'an', 'in']
+      unless little_words.include?(word)
+        word.capitalize!
+      end
+    end
+    words[0].capitalize!
+    words.join(' ')
+  end
 end
